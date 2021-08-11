bb853fbcf4473ca3bfb9d346acc28373d30a1261
Robin Escobar <robinsolises@hotmail.com>
2021-08-10 21:33:12 -0600

    quitado

--- privado-desarrollo/privado-desarrolloFE/core/configGeneral.php
+++
@@ -1,4 +0,0 @@
-<?php
-	const SERVERURL="http://localhost:8044/privado-desarrollo/privado-desarrolloFE/";//Constantes del sistema
-	const COMPANY="PRODUCTOAPP";
-	date_default_timezone_set ("America/Guatemala");
--- privado-desarrollo/privado-desarrolloFE/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once "./core/configGeneral.php";
-	require_once "./controladores/vistasControlador.php";
-//Creamos la instancia
-	$plantilla = new VistasControlador();
-	$plantilla->obtenerPlantillaControlador();
